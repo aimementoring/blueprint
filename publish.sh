a1fb737e132b40c9a0ef0d69485b13cc72e4bfe5
Kevin Bardi <bardi.kevin@gmail.com>
2019-01-22 17:26:07 -0300

    Publish new version of blueprint library 1.0.0

--- publish.sh
+++ publish.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+VERSION=$1
+
 yarn deploy
 git add .
-git commit -m "Publish new version of blueprint library"
+git commit -m "Publish new version of blueprint library $VERSION"
 git push origin master
-npm version
+npm version $VERSION
 npm publish
