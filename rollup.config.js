4ffa2211ca13a2c61f89304b8383011c6160a4d9
Kevin Bardi <bardi.kevin@gmail.com>
2019-01-22 17:43:15 -0300

    Fixes random error on generated index

--- rollup.config.js
+++ rollup.config.js
@@ -8,16 +8,14 @@ import filesize from 'rollup-plugin-filesize';
 import progress from 'rollup-plugin-progress';
 import url from 'rollup-plugin-url';
 import resolve from 'rollup-plugin-node-resolve';
+import external from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
 import svgr from '@svgr/rollup';
-
-// import external from 'rollup-plugin-peer-deps-external';
 
 import pkg from './package.json';
 
 export default {
   input: 'src/lib/index.js',
-  external: ['react', 'styled-jsx/style'],
   output: [
     {
       globals: {
@@ -49,16 +47,11 @@ export default {
       clearLine: false,
     }),
     clean({ targets: 'lib' }),
-    // external(),
-    execute('mkdir dist'),
+    execute('mkdir lib'),
+    external(),
+    resolve(),
     postcss({
       modules: true,
-    }),
-    svgr(),
-    resolve({
-      extensions: ['.js', '.jsx', '.mjs'],
-      main: true,
-      browser: true,
     }),
     commonjs({
       include: ['node_modules/**'],
@@ -73,15 +66,10 @@ export default {
           'Fragment',
         ],
       },
-      sourceMap: false,
     }),
-    // Inline import files as data-URIs or copy them to output
-    // @see https://github.com/rollup/rollup-plugin-url
-    url({
-      limit: 25 * 1024, // inline files < 25k, copy files > 25k
-    }),
+    url(),
+    svgr(),
     babel({
-      // See babel config in `package.json`
       runtimeHelpers: true,
       exclude: 'node_modules/**',
       plugins: ['@babel/plugin-external-helpers'],
