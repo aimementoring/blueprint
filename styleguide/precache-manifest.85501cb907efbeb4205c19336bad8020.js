793e9ad3c89e95ea9fbb7ead6041cdd29210877d
Kevin Bardi <bardi.kevin@gmail.com>
2019-01-31 13:08:22 -0300

    Fixes build

---
+++ styleguide/precache-manifest.85501cb907efbeb4205c19336bad8020.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "cda1caee9bf9bad59dfe",
+    "url": "static/css/main.1ea16a6b.css"
+  },
+  {
+    "revision": "cda1caee9bf9bad59dfe",
+    "url": "build/bundle.cda1caee.js"
+  },
+  {
+    "revision": "f08f1e2d919cf18072fd",
+    "url": "build/1.f08f1e2d.js"
+  },
+  {
+    "revision": "4f865cfb874b935ebf44ae2265cd0a15",
+    "url": "index.html"
+  }
+];
