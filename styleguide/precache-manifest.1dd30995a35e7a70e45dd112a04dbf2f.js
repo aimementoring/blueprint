eaf34f42aa706f338690f405da35b94b0ea7d2e5
David Paley <>
2019-03-22 12:32:20 -0300

    add the deploy files

---
+++ styleguide/precache-manifest.1dd30995a35e7a70e45dd112a04dbf2f.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "47810a18aed6a916c85b",
+    "url": "static/css/main.107e905e.css"
+  },
+  {
+    "revision": "8874d2b227904bb3bd49112bd63cf96b",
+    "url": "index.html"
+  },
+  {
+    "revision": "47810a18aed6a916c85b",
+    "url": "build/bundle.47810a18.js"
+  }
+];
--- styleguide/service-worker.js
+++ styleguide/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.3e2776ede62c91278888438f3a88596d.js"
+  "precache-manifest.1dd30995a35e7a70e45dd112a04dbf2f.js"
 );
 
 workbox.clientsClaim();
