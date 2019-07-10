d4dfeab063de64b356ff88c002f1611a7980ae11
Lara Salameh <hello@larasalameh.com>
2019-07-10 12:34:10 +0100

    Updated Title component to suit design system

--- src/lib/components/title/title.js
+++ src/lib/components/title/title.js
@@ -7,13 +7,20 @@ const titleTypeClass = {
   gradient: styles.gradientTitle,
   blockTitle: styles.blockTitle,
   mainTitle: styles.mainTitle,
+  h1Title: styles.h1Title,
+  h2Title: styles.h2Title,
+  h3Title: styles.h3Title,
+  h4Title: styles.h4Title,
+  h5Title: styles.h5Title,
+  h6Title: styles.h6Title,
 };
 
 export default class Title extends PureComponent {
   static propTypes = {
     ...componentPropTypes,
     text: PropTypes.string,
-    type: PropTypes.oneOf(['gradient', 'blockTitle', 'mainTitle']),
+    // setting up headings as per design system but they will probably change again
+    type: PropTypes.oneOf(['gradient', 'blockTitle', 'mainTitle', 'h1Title', 'h2Title', 'h3Title', 'h4Title', 'h5Title', 'h6Title']),
     children: PropTypes.node,
   };
 
--- src/lib/components/title/title.md
+++ src/lib/components/title/title.md
@@ -11,3 +11,33 @@
   <Title type="mainTitle">Main title</Title>
 // </div>
 ```
+
+
+```js
+<Title type="h1Title">H1 Title</Title>
+```
+
+
+```js
+<Title type="h2Title">H2 Title</Title>
+```
+
+
+```js
+<Title type="h3Title">H3 Title</Title>
+```
+
+
+```js
+<Title type="h4Title">H4 Title</Title>
+```
+
+
+```js
+<Title type="h5Title">H5 Title</Title>
+```
+
+
+```js
+<Title type="h6Title">H6 Title</Title>
+```
