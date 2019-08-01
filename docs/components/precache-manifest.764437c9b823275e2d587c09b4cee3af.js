58e6ca417c03c2874137d4589a2e87f330f4bb7c
Kevin Bardi <bardi.kevin@gmail.com>
2019-08-01 11:25:17 -0300

    Initial docs subtree commit

---
+++ docs/components/precache-manifest.764437c9b823275e2d587c09b4cee3af.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "569f9ea6f49dd642a145",
+    "url": "build/bundle.569f9ea6.js"
+  },
+  {
+    "revision": "b1ab7c12c4bc0e3f06afe621816b0dc1",
+    "url": "index.html"
+  },
+  {
+    "revision": "569f9ea6f49dd642a145",
+    "url": "static/css/main.8f074717.css"
+  }
+]);
