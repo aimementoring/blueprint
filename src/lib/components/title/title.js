5c0a5afc4da2e05d4ca6b1c5b8eaffb7d68e915c
Kevin Bardi <kevinbardi@Kevins-MacBook-Pro-2.local>
2019-10-02 01:42:29 -0300

    Update title component to specify different tags according to title type

--- src/lib/components/title/title.js
+++ src/lib/components/title/title.js
@@ -4,31 +4,45 @@ import styles from './title.module.scss';
 import { componentPropTypes, defaultComponentPropTypes } from '../../utils/componentPropTypes';
 
 const titleTypeClass = {
-  gradient: styles.gradientTitle,
-  blockTitle: styles.blockTitle,
-  mainTitle: styles.mainTitle,
-  h1Title: styles.h1Title,
-  h2Title: styles.h2Title,
-  h3Title: styles.h3Title,
-  h4Title: styles.h4Title,
-  h5Title: styles.h5Title,
-  h6Title: styles.h6Title,
+  gradient: {
+    style: styles.gradientTitle,
+    tag: 'span',
+  },
+  blockTitle: {
+    style: styles.blockTitle,
+    tag: 'div',
+  },
+  mainTitle: {
+    style: styles.mainTitle,
+    tag: 'div',
+  },
+  h1Title: {
+    style: styles.h1Title,
+    tag: 'h1',
+  },
+  h2Title: {
+    style: styles.h2Title,
+    tag: 'h2',
+  },
+  h3Title: {
+    style: styles.h3Title,
+    tag: 'h3',
+  },
+  h4Title: {
+    style: styles.h4Title,
+    tag: 'h4',
+  },
+  h5Title: {
+    style: styles.h5Title,
+    tag: 'h5',
+  },
+  h6Title: {
+    style: styles.h6Title,
+    tag: 'h6',
+  },
 };
 
-export default class Title extends PureComponent {
-  static propTypes = {
-    ...componentPropTypes,
-    text: PropTypes.string,
-    // setting up headings as per design system but they will probably change again
-    type: PropTypes.oneOf(['gradient', 'blockTitle', 'mainTitle', 'h1Title', 'h2Title', 'h3Title', 'h4Title', 'h5Title', 'h6Title']),
-    children: PropTypes.node,
-  };
-
-  static defaultProps = {
-    ...defaultComponentPropTypes,
-    type: 'gradient',
-  };
-
+class Title extends PureComponent {
   render() {
     const {
       text,
@@ -38,12 +52,27 @@ export default class Title extends PureComponent {
       children,
     } = this.props;
 
-    const titleClass = titleTypeClass[type];
+    const { style, tag: Component } = titleTypeClass[type];
 
     return (
       <div className={styles[`theme-${theme}`]}>
-        <span className={`${titleClass} ${className}`}>{children || text}</span>
+        <Component className={`${style} ${className}`}>{children || text}</Component>
       </div>
     );
   }
 }
+
+Title.propTypes = {
+  ...componentPropTypes,
+  text: PropTypes.string,
+  // setting up headings as per design system but they will probably change again
+  type: PropTypes.oneOf(['gradient', 'blockTitle', 'mainTitle', 'h1Title', 'h2Title', 'h3Title', 'h4Title', 'h5Title', 'h6Title']),
+  children: PropTypes.node,
+};
+
+Title.defaultProps = {
+  ...defaultComponentPropTypes,
+  type: 'gradient',
+};
+
+export default Title;
