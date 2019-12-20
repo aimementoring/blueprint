78f80ce26fe8488ae0e581c9b0c9735d085e79a9
Kevin Bardi <bardi.kevin@gmail.com>
2019-12-20 20:51:44 -0300

    Fixes phone number

--- src/lib/components/phoneInput/phoneInput.js
+++ src/lib/components/phoneInput/phoneInput.js
@@ -1,10 +1,12 @@
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
-import ReactPhoneInput from 'react-phone-number-input';
-import { componentPropTypes, defaultComponentPropTypes } from '../../utils/componentPropTypes';
-import { withValidation } from '../../utils/hocs';
-import styles from './phoneInput.module.scss';
-import 'react-phone-number-input/style.css';
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
+import ReactPhoneInput from "react-phone-number-input";
+import {
+  componentPropTypes,
+  defaultComponentPropTypes,
+} from "../../utils/componentPropTypes";
+import { withValidation } from "../../utils/hocs";
+import styles from "./phoneInput.module.scss";
 
 class PhoneInput extends PureComponent {
   static propTypes = {
@@ -21,12 +23,12 @@ class PhoneInput extends PureComponent {
 
   static defaultProps = {
     ...defaultComponentPropTypes,
-    placeholder: 'phone',
+    placeholder: "phone",
     className: styles.input,
-    name: 'phone',
+    name: "phone",
     required: false,
-    defaultCountry: 'auto',
-    value: '',
+    defaultCountry: "auto",
+    value: "",
     onCountrySelected: () => {},
     onChangeFunction: () => {},
     // http://catamphetamine.github.io/react-phone-number-input/docs/styleguide/index.html#phoneinput
@@ -54,21 +56,24 @@ class PhoneInput extends PureComponent {
       onCountrySelected,
       ...inputProps
     } = this.props;
+    // eslint-disable-next-line no-console
+    console.log({ theme, style: styles[`theme-${theme}`], styles });
     return (
-      <div className={styles[`theme-${theme}`]}>
-        <div className={`${containerClassName} ${styles.inputWrapper}`}>
-          <ReactPhoneInput
-            country={defaultCountry}
-            placeholder={placeholder}
-            value={value}
-            onChange={this.handleChange}
-            error={isValidationOk() ? '' : getValidationMessage()}
-            className={className}
-            onCountryChange={onCountrySelected}
-            {...inputProps}
-          />
-          {/* {renderValidationError()} */}
-        </div>
+      <div
+        className={`${containerClassName} ${styles[`theme-${theme}`]} ${
+          styles.inputWrapper
+        }`}
+      >
+        <ReactPhoneInput
+          country={defaultCountry}
+          placeholder={placeholder}
+          value={value}
+          onChange={this.handleChange}
+          error={isValidationOk() ? "" : getValidationMessage()}
+          className={className}
+          onCountryChange={onCountrySelected}
+          {...inputProps}
+        />
       </div>
     );
   }
