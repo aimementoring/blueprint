d70328a3a31469e7d183620c0d0625e7e81d7356
Rin Raeuber <mail@rin-raeuber.com>
2020-01-27 15:11:07 +0700

    Feature: Add new Loading spinner.

--- src/lib/components/loading/loading.js
+++ src/lib/components/loading/loading.js
@@ -15,20 +15,29 @@ export default class Loading extends PureComponent {
   };
 
   render() {
-    const { loading, theme, className, containerClassName } = this.props;
+    const { loading, theme } = this.props;
 
     if (loading) {
       return (
         <div className={styles[`theme-${theme}`]}>
-          <div className={`${styles.loading} ${containerClassName}`}>
-            <div className={`${styles.spin} ${styles.spinning} ${className}`}>
-              <span className={`${styles.spinDot} ${styles.spinDotSpin}`}>
-                <i />
-                <i />
-                <i />
-                <i />
-              </span>
-            </div>
+          <div className={styles.loading}>
+            <svg className={styles.loadingSvg} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1000">
+              <defs>
+                <linearGradient id="gradient" x1="0%" y1="0%" x2="0%" y2="100%">
+                  <stop offset="0%" stop-color="#9135F0" />
+                  <stop offset="100%" stop-color="#5eaefd" />
+                </linearGradient>
+              </defs>
+              <title>Loader</title>
+              <g>
+                <path className={`${styles.loader} ${styles.letterA}`} d="M76.92,469.91C76.92,219,280.35,15.53,531.3,15.53S985.68,219,985.68,469.91,782.25,924.29,531.3,924.29C365.73,924.29,132,773.82,178,626.51c0,0,96.6-318.12,120.56-329.3,33.49-15.62-.47,121.68,35.25,300.28" />
+                <path className={`${styles.loader} ${styles.letterADash}`} d="M192.51,507.07s151.26-84.88,183.63-117.25" />
+                <path className={`${styles.loader} ${styles.letterI}`} d="M435.86,334.05s-38,214.33-36.84,251.16" />
+                <path className={`${styles.loader} ${styles.letterM}`} d="M456,590.79c20.1-27.9,52.56-219.9,73.72-218.79s26.75,168.56,42.38,167.45S668.6,296,694.28,296,648,602.51,659.12,643.82" />
+                <path className={`${styles.loader} ${styles.letterE}`} d="M884.6,359.17s-79.81,29-107.72,46.32c-15.14,9.39-33.24,54.28-44.23,95.79-9.25,35-13.45,67.55-6.56,74.44,15.07,15.07,42.42,36.84,147.35-27.9" />
+                <path className={`${styles.loader} ${styles.letterEDash}`} d="M732.65,501.28C764.05,508.19,850,480.84,850,480.84" />
+              </g>
+            </svg> 
           </div>
         </div>
       );
