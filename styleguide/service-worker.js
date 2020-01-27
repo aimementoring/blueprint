c0f5b4e2e546b78c05ed8bad0081410b857904b3
Rin Raeuber <mail@rin-raeuber.com>
2020-01-27 15:13:32 +0700

    Built.

--- styleguide/service-worker.js
+++ styleguide/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.135528c591f931dffb79888c227c2351.js"
+  "precache-manifest.fd5ed18e0e7361cd31e22fe5f622e956.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
