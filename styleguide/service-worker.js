c5b10cde32c1792ab74954f7084e6aafb7c70e7c
Kevin Bardi <bardi.kevin@gmail.com>
2020-01-28 18:19:45 -0300

    Built library

--- styleguide/service-worker.js
+++ styleguide/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.135528c591f931dffb79888c227c2351.js"
+  "precache-manifest.d53eb76f1f642bdb4d5d3573552ad56a.js"
 );
 
 self.addEventListener('message', (event) => {
