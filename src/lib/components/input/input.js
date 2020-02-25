bd7f8ad43fe1af04f22cde8f5e86026ac3f792ca
Rin Raeuber <mail@rin-raeuber.com>
2020-02-25 15:47:21 +0100

    Fix: Fix validations for input.

--- src/lib/components/input/input.js
+++ src/lib/components/input/input.js
@@ -5,21 +5,27 @@ import {
   defaultComponentPropTypes,
 } from "../../utils/componentPropTypes";
 import { handleInputChange } from "../oldInput/utils.ignore";
+import { withValidation } from '../../utils/hocs';
 import styles from "./input.module.scss";
 
 const Input = ({
   theme,
   name,
   value,
-  error,
   label,
+  error,
   className,
   type,
   onChangeFunction,
-  handleValidations,
   helperText,
   containerClassName,
   placeholder,
+  disabled,
+  autoFocus,
+  // props from withValidation HOC
+  renderValidationError,
+  handleValidations,
+  isValidationOk,
   ...inputProps
 }) => {
   const handleChange = event =>
@@ -35,13 +41,15 @@ const Input = ({
           name={name}
           onChange={handleChange}
           value={value}
+          disabled={disabled ? 'disabled' : ''}
+          autoFocus={autoFocus}
           {...inputProps}
         />
         <label htmlFor={name}>
           <span className={styles.fieldName}>{label || placeholder}</span>
         </label>
-        <span className={`${styles.errorMessage} ${error && styles.active}`}>
-          {error}
+        <span className={`${isValidationOk() && styles.error} ${styles.input} ${className} ${styles.errorMessage} ${error && styles.active}`}>
+          {`${error} ${renderValidationError()}`}
         </span>
         {!error && helperText && (
           <span className={styles.helperMessage}>{helperText}</span>
@@ -60,8 +68,11 @@ Input.propTypes = {
   className: PropTypes.string,
   type: PropTypes.string,
   helperText: PropTypes.string,
-  handleValidations: PropTypes.func,
   onChangeFunction: PropTypes.func,
+  // props from withValidation HOC
+  renderValidationError: PropTypes.func,
+  handleValidations: PropTypes.func.isRequired,
+  isValidationOk: PropTypes.func.isRequired,
 };
 
 Input.defaultProps = {
@@ -70,9 +81,10 @@ Input.defaultProps = {
   error: null,
   className: "",
   type: "text",
+  disabled: false,
   helperText: null,
   handleValidations: () => true,
   onChangeFunction: () => { },
 };
 
-export default Input;
+export default withValidation(Input);
