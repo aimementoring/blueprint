355cb13fb3a3f7e1653c7ba74134f12c7ba5cdd5
davidpaley <david.paleyy@gmail.com>
2020-03-03 20:12:56 -0300

    fix: input type date bad behaviour

--- src/lib/components/input/utils.ignore.js
+++ src/lib/components/input/utils.ignore.js
@@ -1,6 +1,12 @@
-export const handleInputChange = (event, name, handleValidations, onChangeFunction) => {
-  const target = event.target;
-  const value = target.type === 'checkbox' ? target.checked : target.value;
+export const handleInputChange = (
+  event,
+  name,
+  handleValidations,
+  onChangeFunction,
+) => {
+  const { type, checked } = event.target;
+  const value = type === 'checkbox' ? checked : event.target.value;
+  if (type === 'date' && !value) return;
   const isWrongValidation = handleValidations(value);
   onChangeFunction(name, value, isWrongValidation);
 };
