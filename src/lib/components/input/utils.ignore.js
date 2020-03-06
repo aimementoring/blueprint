33844768e29f4fa1597dc9f8b4ec1b596af97f80
Rin Raeuber <mail@rin-raeuber.com>
2020-03-06 12:23:04 +0100

    Fix: Let people empty date fields.

--- src/lib/components/input/utils.ignore.js
+++ src/lib/components/input/utils.ignore.js
@@ -6,7 +6,6 @@ export const handleInputChange = (
 ) => {
   const { type, checked } = event.target;
   const value = type === 'checkbox' ? checked : event.target.value;
-  if (type === 'date' && !value) return;
   const isWrongValidation = handleValidations(value);
   onChangeFunction(name, value, isWrongValidation);
 };
